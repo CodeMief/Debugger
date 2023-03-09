61c6cbc1aab3d2d50427e54568bd64f9b693243e
Yoran <457036@fontys.student.nl>
2023-03-09 16:06:34 +0100

    added enemy 1 plus some other changes surrounding it

---
+++ Assets/Scripts/Enemy/EnemyV1/EnemyV1Attack.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] int damage = 25;
+    HealthBehaviour target;
+    Random rand = new Random();
+
+    void Start()
+    {
+        target = FindObjectOfType<HealthBehaviour>();
+    }
+
+    public void AttackHitEvent()
+    {
+        if(target == null) return;
+        if (rand.Next(0, 2) == 0)
+        {
+            target.TakeDamage(damage);
+        }
+        else return;
+    }
+}
--- Assets/Scripts/Scriptable Objects/StatsSO.cs
+++ Assets/Scripts/Scriptable Objects/StatsSO.cs
@@ -8,8 +8,10 @@ public class StatsSO : ScriptableObject
     [SerializeField] private int health;
     [SerializeField] private float movementSpeed;
     [SerializeField] private float jumpHeight;
+    [SerializeField] private float stoppingDistance;
 
     public int Health { get => health; }
     public float MovementSpeed { get => movementSpeed; }
     public float JumpHeight { get => jumpHeight; }
+    public float StoppingDistance { get => stoppingDistance; }
 }
