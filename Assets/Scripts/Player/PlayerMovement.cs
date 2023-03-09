8c3979d2ba1a326f6ab5c73f8ff56219baa4e4c6
Geerman,Genelle G.N.J <g.geerman@student.fontys.nl>
2023-03-09 12:32:36 +0100

    Fixed a camera issue

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -27,14 +27,18 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         Move();
+        Rotate();
     }
 
     private void Move()
     {
-        Vector3 dir = cam.forward * position.z + cam.right * position.x;
+        Vector3 dir = transform.forward * position.z + cam.right * position.x;
         rigidbody.MovePosition(transform.position + dir * stats.MovementSpeed * Time.deltaTime);
     }
-
+    private void Rotate()
+    {
+        transform.rotation = Quaternion.AngleAxis(cam.eulerAngles.y, Vector3.up);
+    }
     //Event System
     private void OnMove(Vector3 value)
     {
